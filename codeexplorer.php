15ad333d900ca945f04b6ab13e5706e0bd1a7a8b
Daniel Marschall <info@daniel-marschall.de>
2017-12-17 00:24:10 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/fastphp/trunk@37 4275edf5-accd-43b1-89a6-70d33797d1ed

--- codeexplorer.php
+++ codeexplorer.php
@@ -17,30 +17,25 @@ define('ICON_TYPE_TODO',           'TDO');
 define('ICON_TYPE_ERROR',          'ERR');
 
 while (true) {
-	$lines = array();
-	while ($f = fgets(STDIN)){
-		if (trim($f) == chr(1).chr(2).chr(3).chr(4).chr(5).chr(6).chr(7).chr(8)) break;
-		$lines[] = $f;
-	}
-	$code = implode("", $lines);
+	$code = FastNodeReader::readCodeFromEditor();
 
-	echo "FAST100!\n";
-	
 	$x = token_get_all($code);
-	
-	if (!$x) {
-		echo _outputLeafNode(ICON_TYPE_ERROR.implode('', $icon_add_flags), 1, 'SYNTAX ERROR');
-	}
-	
 	$wait_function = false;
 	$wait_class = false;
 	$wait_trait = false;
 	$wait_interface = false;
 	$wait_abstract_func_list_end = false;
 	$levelAry = array();
-	$icon_add_flags = array('_', '_', '_');
+	$icon = new FastPHPIcon();
 	$dep = 0;
 	$insideFuncAry = array();
+
+	echo FastNodeWriter::outputHeader();
+
+	if (!$x) {
+		$icon->setType(ICON_TYPE_ERROR);
+		echo FastNodeWriter::outputLeafNode($icon, 1, 'SYNTAX ERROR');
+	}
 
 	foreach ($x as $n => $data) {
 		if ($data == '{') $dep++;
@@ -48,7 +43,7 @@ while (true) {
 			$dep--;
 			if ((count($levelAry) > 0) && (array_peek($levelAry) == $dep)) {
 				array_pop($levelAry);
-				echo _outputDecreaseLevel();
+				echo FastNodeWriter::outputDecreaseLevel();
 			}
 			if ((count($insideFuncAry) > 0) && (array_peek($insideFuncAry) == $dep)) {
 				array_pop($insideFuncAry);
@@ -61,7 +56,7 @@ while (true) {
 
 		if ($wait_function && ($token == T_STRING)) {
 			$wait_function = false;
-			if ($icon_add_flags[1] == 'A') {
+			if ($icon->isAbstract()) {
 				$desc = "abstract function $value()";
 				$wait_abstract_func_list_end = true;
 			} else {
@@ -70,31 +65,32 @@ while (true) {
 			}
 
 			if ($value == '__construct') { // TODO: auch eine methode mit dem namen der klasse soll eine konstruktor sein
-				echo _outputLeafNode(ICON_TYPE_CONSTRUCTOR.implode('', $icon_add_flags), $line, $desc);
+                                $icon->setType(ICON_TYPE_CONSTRUCTOR);
 			} else if ($value == '__destruct') {
-				echo _outputLeafNode(ICON_TYPE_DESTRUCTOR.implode('', $icon_add_flags), $line, $desc);
+                                $icon->setType(ICON_TYPE_DESTRUCTOR);
 			} else if (substr($value, 0, 2) == '__') {
-				echo _outputLeafNode(ICON_TYPE_MAGICMETHOD.implode('', $icon_add_flags), $line, $desc);
+                                $icon->setType(ICON_TYPE_MAGICMETHOD);
 			} else {
-				echo _outputLeafNode(ICON_TYPE_FUNCTION.implode('', $icon_add_flags), $line, $desc);
+                                $icon->setType(ICON_TYPE_FUNCTION);
 			}
-
-			$icon_add_flags = array('_', '_', '_');
+			echo FastNodeWriter::outputLeafNode($icon, $line, $desc);
+			$icon->reset();
 		}
 
 		if ($wait_class && ($token == T_STRING)) {
-                        if ($icon_add_flags[1] == 'A') {
-				$desc = "abstract class $value\n";
+                        if ($icon->isAbstract()) {
+				$desc = "Abstract Class $value\n";
 			} else {
-				$desc = "class $value\n";
+				$desc = "Class $value\n";
 			}
 			$wait_class = false;
 			$levelAry[] = $dep;
 
-			echo _outputLeafNode(ICON_TYPE_CLASS.implode('', $icon_add_flags), $line, $desc);
-			$icon_add_flags = array('_', '_', '_');
+			$icon->setType(ICON_TYPE_CLASS);
+			echo FastNodeWriter::outputLeafNode($icon, $line, $desc);
+			$icon->reset();
 
-			echo _outputIncreaseLevel();
+			echo FastNodeWriter::outputIncreaseLevel();
 		}
 
 		if ($wait_trait && ($token == T_STRING)) {
@@ -102,10 +98,11 @@ while (true) {
 			$wait_trait = false;
 			$levelAry[] = $dep;
 
-			echo _outputLeafNode(ICON_TYPE_TRAIT.implode('', $icon_add_flags), $line, $desc);
-			$icon_add_flags = array('_', '_', '_');
+			$icon->setType(ICON_TYPE_TRAIT);
+			echo FastNodeWriter::outputLeafNode($icon, $line, $desc);
+			$icon->reset();
 
-			echo _outputIncreaseLevel();
+			echo FastNodeWriter::outputIncreaseLevel();
 		}
 
 		if ($wait_interface && ($token == T_STRING)) {
@@ -113,37 +110,40 @@ while (true) {
 			$wait_interface = false;
 			$levelAry[] = $dep;
 
-			echo _outputLeafNode(ICON_TYPE_INTERFACE.implode('', $icon_add_flags), $line, $desc);
-			$icon_add_flags = array('_', '_', '_');
+                        $icon->setType(ICON_TYPE_INTERFACE);
+			echo FastNodeWriter::outputLeafNode($icon, $line, $desc);
+			$icon->reset();
 
-			echo _outputIncreaseLevel();
+			echo FastNodeWriter::outputIncreaseLevel();
 		}
 
 		if ($wait_const && ($token == T_STRING)) {
 			$desc = "const $value\n";
 			$wait_const = false;
 
-			echo _outputLeafNode(ICON_TYPE_CONST.implode('', $icon_add_flags), $line, $desc);
-			$icon_add_flags = array('_', '_', '_');
+			$icon->setType(ICON_TYPE_CONST);
+			echo FastNodeWriter::outputLeafNode($icon, $line, $desc);
+			$icon->reset();
 		}
 
 		if ((!$wait_abstract_func_list_end) && (count($levelAry) > 0) && (count($insideFuncAry) == 0) && ($token == T_VARIABLE)) {
 			$desc = "$value\n";
 
-			echo _outputLeafNode(ICON_TYPE_VAR.implode('', $icon_add_flags), $line, $desc);
-			$icon_add_flags = array('_', '_', '_');
+			$icon->setType(ICON_TYPE_VAR);
+			echo FastNodeWriter::outputLeafNode($icon, $line, $desc);
+			$icon->reset();
 		}
 
-		if ($token == T_PRIVATE)   $icon_add_flags[0] = '1';
-		if ($token == T_PROTECTED) $icon_add_flags[0] = '2';
-		if ($token == T_PUBLIC)    $icon_add_flags[0] = '3';
-		if ($token == T_ABSTRACT)  $icon_add_flags[1] = 'A';
-		if ($token == T_FINAL)     $icon_add_flags[1] = 'F';
-		if ($token == T_STATIC)    $icon_add_flags[2] = 'S';
+		if ($token == T_PRIVATE)   $icon->setPrivate();
+		if ($token == T_PROTECTED) $icon->setProtected();
+		if ($token == T_PUBLIC)    $icon->setPublic();
+		if ($token == T_ABSTRACT)  $icon->setAbstract();
+		if ($token == T_FINAL)     $icon->setFinal();
+		if ($token == T_STATIC)    $icon->setStatic();
 
 		if (($data == ';') || ($data == '{') || ($data == '}')) {
 			$wait_abstract_func_list_end = false;
-			$icon_add_flags = array('_', '_', '_');
+			$icon->reset();
 		}
 
 		if ($token == T_FUNCTION) {
@@ -166,73 +166,166 @@ while (true) {
 			$wait_const = true;
 		}
 
-		if ($token == T_COMMENT) {
-			if ((stripos($value, 'TODO')   !== false) ||
-			    (stripos($value, 'BUGBUG') !== false) ||
-			    (stripos($value, 'FIXME')  !== false) ||
-			    (stripos($value, 'XXX')    !== false)) {
-				echo _outputLeafNode(ICON_TYPE_TODO.implode('', $icon_add_flags), $line, strip_comment($value));
-			}
+		if (($token == T_COMMENT) && _isToDoDescription($value)) {
+			// Because a TO-DO-entry can stand everywhere (e.g. between a "private" and a "function")
+			// we shall not alter the $icon instance
+			$todoIcon = clone $icon;
+			$todoIcon->setType(ICON_TYPE_TODO);
+			echo FastNodeWriter::outputLeafNode($todoIcon, $line, strip_comment($value));
 		}
 	}
 	
-	echo _outputExit();
+	echo FastNodeWriter::outputExit();
 
-	echo chr(1).chr(2).chr(3).chr(4).chr(5).chr(6).chr(7).chr(8);
+	echo FastNodeWriter::signalOutputEnd();
 
 	sleep(1);
 }
 
-function _iconCodeToIndex($icon) {
-	$typ = substr($icon, 0, 3);
+function _isToDoDescription($comment) {
+	return ((stripos($value, 'TODO')   !== false) ||
+	        (stripos($value, 'BUGBUG') !== false) ||
+	        (stripos($value, 'FIXME')  !== false) ||
+	        (stripos($value, 'XXX')    !== false));
+}
 
-	if ($icon[3] == '_') $icon[3] = '3'; // public is default visibility
+function _iconCodeToIndex(/* FastPHPIcon */ $icon) {
+	     if (($icon->getType() == ICON_TYPE_CLASS)                               && (!$icon->isAbstract())) return  0;
+	else if (($icon->getType() == ICON_TYPE_CLASS)                               && ( $icon->isAbstract())) return  1;
+	else if (($icon->getType() == ICON_TYPE_INTERFACE)                                                    ) return  1;
+	else if (($icon->getType() == ICON_TYPE_TRAIT)                                                        ) return  2;
+	else if (($icon->getType() == ICON_TYPE_CONST)     && ($icon->isPrivate())                            ) return  3;
+	else if (($icon->getType() == ICON_TYPE_VAR)       && ($icon->isPrivate())                            ) return  4;
+	else if (($icon->isMethod())                       && ($icon->isPrivate())   && (!$icon->isAbstract())) return  5;
+	else if (($icon->isMethod())                       && ($icon->isPrivate())   && ( $icon->isAbstract())) return  6;
+	else if (($icon->getType() == ICON_TYPE_CONST)     && ($icon->isProtected())                          ) return  7;
+	else if (($icon->getType() == ICON_TYPE_VAR)       && ($icon->isProtected())                          ) return  8;
+	else if (($icon->isMethod())                       && ($icon->isProtected()) && (!$icon->isAbstract())) return  9;
+	else if (($icon->isMethod())                       && ($icon->isProtected()) && ( $icon->isAbstract())) return 10;
+	else if (($icon->getType() == ICON_TYPE_CONST)     && ($icon->isPublic())                             ) return 11;
+	else if (($icon->getType() == ICON_TYPE_VAR)       && ($icon->isPublic())                             ) return 12;
+	else if (($icon->isMethod())                       && ($icon->isPublic())    && (!$icon->isAbstract())) return 13;
+	else if (($icon->isMethod())                       && ($icon->isPublic())    && ( $icon->isAbstract())) return 14;
+	else if (($icon->getType() == ICON_TYPE_TODO)                                                         ) return 15;
+	else                                                                                                    return -1;
+}
 
-	if (($typ == 'MAG') || ($typ == 'CST') || ($typ == 'DST')) {
-		$typ = 'FUN';
-	} else if ($typ == 'INT') {
-		$typ = 'CLS';
-		$icon[4] = 'A';
+class FastPHPIcon {
+	private $type;          // ICON_TYPE_*
+	private $visibility;    // 1=private, 2=protected, 3=public(default)
+	private $abstractFinal; // 'A', 'F', ''(default)
+	private $static;        // true, false(default)
+
+	public function setType($type) {
+		$this->type = $type;
+	}
+	public function getType() {
+		return $this->type;
 	}
 
-	     if (($typ == 'CLS')                      && ($icon[4] != 'A')) return  0;
-	else if (($typ == 'CLS')                      && ($icon[4] == 'A')) return  1;
-	else if (($typ == 'TRA')                                          ) return  2;
-	else if (($typ == 'CON') && ($icon[3] == '1')                     ) return  3;
-	else if (($typ == 'VAR') && ($icon[3] == '1')                     ) return  4;
-	else if (($typ == 'FUN') && ($icon[3] == '1') && ($icon[4] != 'A')) return  5;
-	else if (($typ == 'FUN') && ($icon[3] == '1') && ($icon[4] == 'A')) return  6;
-	else if (($typ == 'CON') && ($icon[3] == '2')                     ) return  7;
-	else if (($typ == 'VAR') && ($icon[3] == '2')                     ) return  8;
-	else if (($typ == 'FUN') && ($icon[3] == '2') && ($icon[4] != 'A')) return  9;
-	else if (($typ == 'FUN') && ($icon[3] == '2') && ($icon[4] == 'A')) return 10;
-	else if (($typ == 'CON') && ($icon[3] == '3')                     ) return 11;
-	else if (($typ == 'VAR') && ($icon[3] == '3')                     ) return 12;
-	else if (($typ == 'FUN') && ($icon[3] == '3') && ($icon[4] != 'A')) return 13;
-	else if (($typ == 'FUN') && ($icon[3] == '3') && ($icon[4] == 'A')) return 14;
-	else if (($typ == 'TDO')                                          ) return 15;
-	else                                                                return -1;
+	public function setAbstract() {
+		$this->abstractFinal = 'A';
+	}
+	public function isAbstract() {
+		return $this->abstractFinal == 'A';
+	}
+
+	public function setFinal() {
+		$this->abstractFinal = 'F';
+	}
+	public function isFinal() {
+		return $this->abstractFinal == 'F';
+	}
+
+	public function setStatic() {
+		$this->static = true;
+	}
+	public function isStatic() {
+		return $this->static;
+	}
+
+	public function setPrivate() {
+		$this->visibility = 1;
+	}
+	public function isPrivate() {
+		return $this->visibility == 1;
+	}
+
+	public function setProtected() {
+		$this->visibility = 2;
+	}
+	public function isProtected() {
+		return $this->visibility == 2;
+	}
+
+	public function setPublic() {
+		$this->visibility = 3;
+	}
+	public function isPublic() {
+		return $this->visibility == 3;
+	}
+
+	public function isMethod() {
+		return (($this->type == ICON_TYPE_FUNCTION)    ||
+                        ($this->type == ICON_TYPE_CONSTRUCTOR) ||
+                        ($this->type == ICON_TYPE_DESTRUCTOR)  ||
+                        ($this->type == ICON_TYPE_MAGICMETHOD));
+	}
+
+	public function reset() {
+		$this->type          = null;
+		$this->visibility    = 3;
+		$this->abstractFinal = '';
+		$this->static        = false;
+	}
+
+	public function __construct() {
+		$this->reset();
+	}
 }
 
-function _outputLeafNode($icon, $line, $description) {
-	$imageindex = _iconCodeToIndex($icon);
-	return 'N'.($imageindex == -1 ? '____' : sprintf('%04s', $imageindex)).
-	           sprintf('%08s', $line).
-	           sprintf('%04s', strlen(trim($description))).
-	           trim($description).
-		   "\n";
+class FastNodeReader {
+	public static function readCodeFromEditor() {
+		$lines = array();
+		while ($f = fgets(STDIN)){
+			if (trim($f) == chr(1).chr(2).chr(3).chr(4).chr(5).chr(6).chr(7).chr(8)) break;
+			$lines[] = $f;
+		}
+		return implode("", $lines);
+	}
 }
 
-function _outputIncreaseLevel() {
-	return 'I'."\n";
-}
+class FastNodeWriter {
 
-function _outputDecreaseLevel() {
-	return 'D'."\n";
-}
+	public static function outputLeafNode(/* FastPHPIcon */ $icon, $lineNo, $description) {
+		$imageindex = _iconCodeToIndex($icon);
+		return 'N'.($imageindex == -1 ? '____' : sprintf('%04s', $imageindex)).
+		           sprintf('%08s', $lineNo).
+		           sprintf('%04s', strlen(trim($description))).
+		           trim($description).
+			   "\n";
+	}
 
-function _outputExit() {
-	return 'X'."\n";
+	public static function outputIncreaseLevel() {
+		return 'I'."\n";
+	}
+
+	public static function outputDecreaseLevel() {
+		return 'D'."\n";
+	}
+
+	public static function outputExit() {
+		return 'X'."\n";
+	}
+
+	public static function signalOutputEnd() {
+		return chr(1).chr(2).chr(3).chr(4).chr(5).chr(6).chr(7).chr(8);
+	}
+
+	public static function outputHeader() {
+		return "FAST100!";
+	}
+
 }
 
 function array_peek($array) {
